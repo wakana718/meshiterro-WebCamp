68a612d58153189e809911a366fe2fa998e024a3
EC2 Default User <ec2-user@ip-172-31-70-62.ec2.internal>
2020-10-19 08:50:42 +0000

    [FINISH]5-6

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '094965967e59b8368ef5ce06b472a048a9bdbd280733d691b404485c053e8dee45bd27799bc3db22350cfe7f48514ce4b474eedc0625aac38474ffb1817ddeb3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'post_images#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 end
